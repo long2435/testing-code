8a4a4f841b52e9e70ff3d9cc85519340c5929d4e
Nguyen Nhat Long <nguyennhatlong@example.com>
2025-05-31 10:16:56 +0700

    fix-bug09

--- frontend/src/pages/Auth/Signup/validations.js
+++ frontend/src/pages/Auth/Signup/validations.js
@@ -1,13 +1,11 @@
 import * as Yup from "yup";
 
 const validations = Yup.object().shape({
-
-  
   email: Yup.string()
     .email("Email không hợp lệ")
     .required("Email là bắt buộc"),
   password: Yup.string()
-    .min(6, "Mật khẩu phải có ít nhất 8 ký tự")
+    .min(8, "Mật khẩu phải có ít nhất 8 ký tự")
     .required("Mật khẩu là bắt buộc"),
   passwordConfirm: Yup.string()
     .oneOf([Yup.ref("password"), null], "Mật khẩu xác nhận không khớp")
